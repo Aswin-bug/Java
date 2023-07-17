6c9dd586cbdbf77f8c906479cfd36d6ff1b45fa5
Aswin <137083309+Aswin-bug@users.noreply.github.com>
2023-07-17 19:19:18 +0530

    Finding Maximum Sum in a 2D rray

---
+++ MaximumArray.java
@@ -0,0 +1,17 @@
+public class MaximumArray {
+    public static void main(String[] args) {
+        int arr[][]={ {1,5},{7,3},{3,5} };
+        int target=0,inbetween;
+        for (int[] i : arr)
+        {
+            inbetween = 0;
+            for (int j : i)
+            {
+                inbetween += j;
+            }
+            if (inbetween > target)
+                target = inbetween;
+        }
+        System.out.println(target);
+    }
+}
