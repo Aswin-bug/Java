323660303c49fe008eb7fe93f13d38106227c446
Aswin <137083309+Aswin-bug@users.noreply.github.com>
2023-07-28 14:17:38 +0530

    Searching the elements in Two Dimensional Array

---
+++ Binary2D.java
@@ -0,0 +1,29 @@
+
+import java.util.Arrays;
+
+public class Binary2D {
+    public static void main(String[] args) {
+ int arr[][]={  {10,20,30,40},{15,25,35,45},{28,29,37,49},{33,34,38,50} };
+        System.out.println(Arrays.toString(search(arr,34)));
+    }
+    static int[] search(int arr[][],int target)
+    {
+        int row=0;
+        int col=arr.length-1;
+        while(row<arr.length && col>=0)//here col determines last 40th value column , we need to go till(0,0)
+        {
+            if(arr[row][col]==target)
+            {
+                return new int[]{row,col};
+            }
+            if(arr[row][col]>target)
+            {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+     return new int[]{-1,-1};
+    }
+}
