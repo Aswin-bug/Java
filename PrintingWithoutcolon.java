b661496cf37f7f370408282685baa2122cd901eb
Aswin <137083309+Aswin-bug@users.noreply.github.com>
2023-08-23 20:39:21 +0530

    Printing without semicolon

---
+++ PrintingWithoutcolon.java
@@ -0,0 +1,11 @@
+public class PrintingWithoutcolon
+{
+    public static void main(String[] args)
+     {
+        if(System.out.printf("Printing without colon")==null)
+        {
+            
+        }
+        
+    }
+}
