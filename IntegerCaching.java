c89daa677393d9cacd1a493db9945c49c70c66b8
Aswin <137083309+Aswin-bug@users.noreply.github.com>
2023-08-25 13:12:52 +0530

    Limit of Integer

---
+++ IntegerCaching.java
@@ -0,0 +1,10 @@
+public class IntegerCaching {
+    public static void main(String[] args) {
+
+        //here the Integer range is from -128 to 127 so if u give more than that it will show u error this is called Integer Caching.
+
+        Integer n1=128;
+        Integer n2=128;
+        System.out.println(n1==n2?"equal":"not equal");
+    }
+}
